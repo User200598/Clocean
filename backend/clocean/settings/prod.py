de9e3899468aa41e646a828693a3b68960e51635
anand <anandsingh8448535943@gmail.com>
2021-12-08 19:40:17 +0530

    deploy1-req

--- backend/clocean/settings/prod.py
+++ backend/clocean/settings/prod.py
@@ -1,5 +1,11 @@
 import os;
 
+ALLOWED_HOSTS = ['https://clocean.herokuapp.com/',
+                'http://clocean.herokuapp.com/admin',
+                'clocean.herokuapp.com/admin',
+                'clocean.herokuapp.com/admin/']
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
