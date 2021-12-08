29b1cd1ccc9d3d5b64a1131c78aaaa93d26c70eb
anand <anandsingh8448535943@gmail.com>
2021-12-08 19:40:05 +0530

    deploy1-req

--- backend/clocean/settings/dev.py
+++ backend/clocean/settings/dev.py
@@ -1,3 +1,6 @@
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://clocean.herokuapp.com/',
+                'http://clocean.herokuapp.com/admin',
+                'clocean.herokuapp.com/admin',
+                'clocean.herokuapp.com/admin/']
 
 CORS_ORIGIN_ALLOW_ALL = True
